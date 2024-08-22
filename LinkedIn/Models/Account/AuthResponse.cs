431685c96ec80d21f07a9cc02dcfcefb718c4c7e
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-22 13:46:09 +0200

    Added AuthResponse.

---
+++ LinkedIn/Models/Account/AuthResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkedIn.Models.Account
+{
+    public class AuthResponse
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+        public string? Type { get; set; }
+    }
+}
