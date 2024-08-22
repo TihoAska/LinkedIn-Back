e58888530e7af13a6727cc4d149b7bafd5c360c5
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-22 13:47:07 +0200

    New Language models.

---
+++ LinkedIn/Models/ProfileDetails/Languages/Languages.cs
@@ -0,0 +1,17 @@
+﻿using LinkedIn.Models.Users;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinkedIn.Models.ProfileDetails.Languages
+{
+    public class UserLanguages
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Proficiency { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
---
+++ LinkedIn/Models/ProfileDetails/Languages/LanguagesCreateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkedIn.Models.ProfileDetails.Languages
+{
+    public class LanguagesCreateRequest
+    {
+        public string Name { get; set; }
+        public string Proficiency { get; set; }
+
+    }
+}
