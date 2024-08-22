22e96eb6e269540f80bda3b00452cfbeeb9572ab
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-22 13:45:35 +0200

    Mapped new requests to models.

---
+++ LinkedIn/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using LinkedIn.Models.Pages;
+using LinkedIn.Models.ProfileDetails.Educations;
+using LinkedIn.Models.ProfileDetails.Experiences;
+using LinkedIn.Models.ProfileDetails.Languages;
+using LinkedIn.Models.ProfileDetails.LicensesAndCerfitications;
+using LinkedIn.Models.ProfileDetails.Locations;
+using LinkedIn.Models.ProfileDetails.Skills;
+using LinkedIn.Models.Users;
+
+namespace LinkedIn.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserCreateRequest, User>();
+
+            CreateMap<UserUpdateRequest, User>();
+
+            CreateMap<PageCreateRequest, Page>();
+
+            CreateMap<EducationCreateRequest, UserEducation>();
+
+            CreateMap<ExperienceCreateRequest, Experience>();
+
+            CreateMap<LanguagesCreateRequest, UserLanguages>();
+
+            CreateMap<LicensesCreateRequest, LicensesAndCertifications>();
+
+            CreateMap<CompanyLocationCreateRequest, CompanyLocation>();
+
+            CreateMap<SkillsCreateRequest, UserSkills>();
+        }
+    }
+}
