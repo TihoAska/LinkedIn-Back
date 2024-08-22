72c96bd7b059010d25e0d5f7db43e82dac05243e
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-22 13:46:58 +0200

    New Experience models.

---
+++ LinkedIn/Models/ProfileDetails/Experiences/Experience.cs
@@ -0,0 +1,27 @@
+﻿using LinkedIn.Models.Pages;
+using LinkedIn.Models.ProfileDetails.Locations;
+using LinkedIn.Models.Users;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinkedIn.Models.ProfileDetails.Experiences
+{
+    public class Experience
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public string Position { get; set; }
+        public Page Company { get; set; }
+        public string CompanyImageUrl { get; set; } = "../../../assets/images/pageLogos/default-experience.png";
+        public string EmploymentType { get; set; } = "Full-time";
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int CompanyLocationId { get; set; }
+
+        [ForeignKey("CompanyLocationId")]
+        public CompanyLocation Location { get; set; }
+        public bool IsRemote { get; set; }
+    }
+}
---
+++ LinkedIn/Models/ProfileDetails/Experiences/ExperienceCreateRequest.cs
@@ -0,0 +1,18 @@
+﻿
+
+using LinkedIn.Models.ProfileDetails.Locations;
+
+namespace LinkedIn.Models.ProfileDetails.Experiences
+{
+    public class ExperienceCreateRequest
+    {
+        public int UserId { get; set; }
+        public string Position { get; set; }
+        public string CompanyName { get; set; }
+        public string EmploymentType { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public CompanyLocation Location { get; set; }
+        public bool IsRemote { get; set; }
+    }
+}
