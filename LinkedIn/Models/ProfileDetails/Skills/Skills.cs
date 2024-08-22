6353724f6c93aaa40a00a206b4d47da407a576ca
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-22 13:47:36 +0200

    New Skills models.

---
+++ LinkedIn/Models/ProfileDetails/Skills/Skills.cs
@@ -0,0 +1,17 @@
+﻿using LinkedIn.Models.Users;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinkedIn.Models.ProfileDetails.Skills
+{
+    public class UserSkills
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SkillsImageUrl { get; set; }
+    }
+}
---
+++ LinkedIn/Models/ProfileDetails/Skills/SkillsCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedIn.Models.ProfileDetails.Skills
+{
+    public class SkillsCreateRequest
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CompanyNameWhereSkillWasEarned { get; set; }
+    }
+}
