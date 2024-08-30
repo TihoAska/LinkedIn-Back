4b971994a8163fb062ee13ba9c92aa4acd270cf6
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-30 19:16:48 +0200

    New migrations.

---
+++ LinkedIn/Migrations/20240825140605_ChangedIsRemoteToLocationType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkedIn.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedIsRemoteToLocationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRemote",
+                table: "Experiences");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationType",
+                table: "Experiences",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationType",
+                table: "Experiences");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRemote",
+                table: "Experiences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
---
+++ LinkedIn/Migrations/20240827135852_AddedCompanyIdToExperience.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkedIn.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCompanyIdToExperience : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
