6b4357f9dc90b0ad02dab03af55bff9e8ff1db79
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-30 19:18:35 +0200

    New ICompanyLocationsRepository.

---
+++ LinkedIn/Repository/IRepository/ICompanyLocationsRepository.cs
@@ -0,0 +1,10 @@
+﻿using LinkedIn.Models.ProfileDetails.Locations;
+
+namespace LinkedIn.Repository.IRepository
+{
+    public interface ICompanyLocationsRepository : IGenericRepository<CompanyLocation>
+    {
+        Task<CompanyLocation> GetCompanyLocationByCityName(string cityName, CancellationToken cancellationToken);
+        Task<CompanyLocation> GetCompanyLocationByLocationId(int locationId, CancellationToken cancellationToken);
+    }
+}
