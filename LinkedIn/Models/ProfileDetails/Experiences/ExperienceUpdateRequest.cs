b9e8ce0c2eed4a8ed410f75ae6dd7a75335b7d0f
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-30 19:18:18 +0200

    New ExperienceUpdateRequest.

---
+++ LinkedIn/Models/ProfileDetails/Experiences/ExperienceUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿using LinkedIn.Models.ProfileDetails.Locations;
+
+namespace LinkedIn.Models.ProfileDetails.Experiences
+{
+    public class ExperienceUpdateRequest
+    {
+        public int Id { get; set; }
+        public string Position { get; set; }
+        public string EmploymentType { get; set; }
+        public string CompanyName { get; set; }
+        public CompanyLocation Location { get; set; }
+        public string LocationType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
