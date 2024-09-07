d4c26a18eafb2ceea310a30bb926b4e8c83e9049
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-09-07 20:58:46 +0200

    New LanguageUpdateRequest.

---
+++ LinkedIn/Models/ProfileDetails/Languages/LanguagesUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkedIn.Models.ProfileDetails.Languages
+{
+    public class LanguagesUpdateRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Proficiency { get; set; }
+    }
+}
