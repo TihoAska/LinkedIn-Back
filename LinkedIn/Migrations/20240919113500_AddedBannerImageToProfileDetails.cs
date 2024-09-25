4a7b160a686e10c88e2e78e10fa9ca6694cea250
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-09-25 16:21:47 +0200

    New migrations.

---
+++ LinkedIn/Migrations/20240919113500_AddedBannerImageToProfileDetails.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkedIn.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBannerImageToProfileDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BannerImage",
+                table: "ProfileDetails",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BannerImage",
+                table: "ProfileDetails");
+        }
+    }
+}
